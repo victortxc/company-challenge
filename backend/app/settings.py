beff0773e66bd1582502d0141114981b0db8c7ac
Victor Medeiros <victormedeiros.pj@gmail.com>
2022-07-17 13:17:14 -0300

    change font family

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -131,9 +131,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'web/static'),
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
