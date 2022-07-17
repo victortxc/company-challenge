6173c6fb26f333a03c6b7c385a9f9b80bbe38d8d
Victor Medeiros <victormedeiros.pj@gmail.com>
2022-07-17 01:24:34 -0300

    adding static files

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -129,3 +129,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'web/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
